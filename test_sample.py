69e631593d60927c94070ab921bc71403a118712
skovorodil <60383677+skovorodil@users.noreply.github.com>
2020-04-07 13:09:38 +0300

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,14 +1,6 @@
 from selenium import webdriver
 driver = webdriver.Chrome("chromedriver")
 EMAIL = "zenit1032@gmail.com"
-def test_login() :
-      open()
-      driver.find_element_by_id("email").send_keys(EMAIL)
-      driver.find_element_by_id("pas").send_keys("123456")
-      driver.find_element_by_id("login").click()
-      currentUrl = driver.current_url
-      assert(currentUrl == ("https://eng-slang.000webhostapp.com/main.php")), "Oh no! This assertion failed!"
-      print ("OK")
 def test_reg() :
       open()
       driver.find_element_by_id("reg").click()
@@ -19,6 +11,14 @@ def test_reg() :
       driver.find_element_by_id("regButton").click()
       currentUrl = driver.current_url
       assert(currentUrl == ("https://eng-slang.000webhostapp.com/welcome.php")), "Oh no! This assertion failed!"
+      print ("OK")
+def test_login() :
+      open()
+      driver.find_element_by_id("email").send_keys(EMAIL)
+      driver.find_element_by_id("pas").send_keys("1234")
+      driver.find_element_by_id("login").click()
+      currentUrl = driver.current_url
+      assert(currentUrl == ("https://eng-slang.000webhostapp.com/main.php")), "Oh no! This assertion failed!"
       print ("OK")
       
 def open() :
