e70bc6eb453d562099f4b79f2b868237fb18e823
skovorodil <60383677+skovorodil@users.noreply.github.com>
2020-04-07 13:28:30 +0300

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -7,20 +7,20 @@ driver = webdriver.Chrome("chromedriver")
 EMAIL = "zenit1032@gmail.com"
 def test_reg() :
       open()
-      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "reg"))).click()
-      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "regEmail"))).send_keys("test"+str(random.randint(0, 1000))+"@gmail.com")
-      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "regName"))).send_keys("Вадим"+str(random.randint(0, 1000)))
-      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "regPhone"))).send_keys(str(random.randint(0, 100000)))
-      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "regPas"))).send_keys("12345")
-      WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.ID, "regButton"))).click()
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "reg"))).click()
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "regEmail"))).send_keys("test"+str(random.randint(0, 1000))+"@gmail.com")
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "regName"))).send_keys("Вадим"+str(random.randint(0, 1000)))
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "regPhone"))).send_keys(str(random.randint(0, 100000)))
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "regPas"))).send_keys("12345")
+      WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "regButton"))).click()
       currentUrl = driver.current_url
       assert(currentUrl == ("https://eng-slang.000webhostapp.com/welcome.php")), "Oh no! This assertion failed!"
       print ("OK")
 def test_login() :
       open()
-      driver.find_element_by_id("email").send_keys(EMAIL)
-      driver.find_element_by_id("pas").send_keys("1234")
-      driver.find_element_by_id("login").click()
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "email"))).send_keys(EMAIL)
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "pas"))).send_keys("1234")
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login"))).click()
       currentUrl = driver.current_url
       assert(currentUrl == ("https://eng-slang.000webhostapp.com/main.php")), "Oh no! This assertion failed!"
       print ("OK")
