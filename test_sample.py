cdb7844f112441a7f4baa59794c364d9fb752334
skovorodil <60383677+skovorodil@users.noreply.github.com>
2020-04-07 12:41:11 +0300

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -4,7 +4,7 @@ EMAIL = "zenit1032@gmail.com"
 def test_login() :
       open()
       driver.find_element_by_id("email").send_keys(EMAIL)
-      driver.find_element_by_id("pas").send_keys("12345")
+      driver.find_element_by_id("pas").send_keys("1234")
       driver.find_element_by_id("login").click()
       currentUrl = driver.current_url
       assert(currentUrl == ("https://eng-slang.000webhostapp.com/main.php")), "Oh no! This assertion failed!"
